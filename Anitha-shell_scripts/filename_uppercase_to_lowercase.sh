c043abe892de9b125bcf3ff7cb213b7461407d4e
anithabh83 <anithabh83@gmail.com>
2025-10-20 22:04:30 +0530

    Delete Anitha-shell_scripts directory

--- Anitha-shell_scripts/filename_uppercase_to_lowercase.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Enter file or directory name:"
-read name
-
-lower=$(echo "$name" | tr 'A-Z' 'a-z')  # convert to lowercase
-mv "$name" "$lower"                      # rename
-echo "Renamed '$name' to '$lower'"
-
--- Anitha-shell_scripts/reverse_order.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-count=$#
-
-while [ $count -gt 0 ] 
-do
-	echo "${!count}"
-	count=$(($count - 1))
-done
